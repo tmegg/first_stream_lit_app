b0c5b294b38beba1eee3c4129b491b64a2d6fefa
tmegg <67189710+tmegg@users.noreply.github.com>
2023-08-25 19:48:44 +0200

    adding menu header and 3 points

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Eggs')
