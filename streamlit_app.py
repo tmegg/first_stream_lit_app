59891335d31680ec7200e6854215a1046fbd0ede
tmegg <67189710+tmegg@users.noreply.github.com>
2023-08-25 19:51:32 +0200

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal😊😊😊😊😊😊😊😊☆*: .｡. o(≧▽≦)o .｡.:*☆🤣😂')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Eggs')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
