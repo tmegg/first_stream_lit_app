26ce59ce3eb909b0d3ad5fe90fd0356800d45711
tmegg <67189710+tmegg@users.noreply.github.com>
2023-08-27 17:08:53 +0200

    Update streamlit_app.py
    daraframe update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast menu')
@@ -7,3 +9,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Eggs')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
