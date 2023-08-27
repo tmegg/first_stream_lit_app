aa48a152f695f7f25ebf5b473386cc106f8e750b
tmegg <67189710+tmegg@users.noreply.github.com>
2023-08-27 17:17:14 +0200

    Update streamlit_app.py
    avocado strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ streamlit.text('Hard-boiled Free-range Eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
